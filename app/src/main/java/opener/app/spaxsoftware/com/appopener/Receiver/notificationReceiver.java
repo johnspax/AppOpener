4050f87a0bc29524e509dc36ea6e53eb462fb9e4
Johnspax <johnspax@gmail.com>
2019-01-17 10:38:30 +0300

    Added support for Android O and Android Pie

--- app/src/main/java/opener/app/spaxsoftware/com/appopener/Receiver/notificationReceiver.java
+++ app/src/main/java/opener/app/spaxsoftware/com/appopener/Receiver/notificationReceiver.java
@@ -1,10 +1,15 @@
 package opener.app.spaxsoftware.com.appopener.Receiver;
 
+import android.app.NotificationManager;
 import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+
+import opener.app.spaxsoftware.com.appopener.Alarm.Alarm;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class notificationReceiver extends BroadcastReceiver {
     Context ctx;
@@ -19,15 +24,21 @@ public class notificationReceiver extends BroadcastReceiver {
         String packageName = "opener.app.spaxsoftware.com.appopener";
         try {
             //Open the specific App Info page:
-            Intent intent = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            /*Intent intent = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            intent.addCategory("android.intent.category.DEFAULT");
+            intent.putExtra("Notification Receiver", "Open package " + packageName);
+            intent.setClass(ctx, notificationReceiver.class);
             intent.setData(Uri.parse("package:" + packageName));
-            ctx.startActivity(intent);
+            ctx.startActivity(intent);*/
+            NotificationManager manager = (NotificationManager) ctx.getSystemService(NOTIFICATION_SERVICE);
+            manager.cancel(1);
 
         } catch ( ActivityNotFoundException e ) {
             //Open the generic Apps page:
-            Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_APPLICATIONS_SETTINGS);
-            ctx.startActivity(intent);
-
+            /*Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_APPLICATIONS_SETTINGS);
+            ctx.startActivity(intent);*/
+            NotificationManager manager = (NotificationManager) ctx.getSystemService(NOTIFICATION_SERVICE);
+            manager.cancel(1);
         }
     }
 }
